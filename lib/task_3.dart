d255c8ad46ca26a42e58465a1a0b735e234903ea
atef5922 <sifat15-5922@diu.edu.bd>
2025-04-11 14:05:22 +0600

    Task_3

---
+++ lib/task_3.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ListView Example',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: ListViewExample(),
+    );
+  }
+}
+
+class ListViewExample extends StatelessWidget {
+  final List<int> items = List<int>.generate(100, (index) => index + 1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('ListView Example')),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(title: Text('Item ${items[index]}'));
+        },
+      ),
+    );
+  }
+}
