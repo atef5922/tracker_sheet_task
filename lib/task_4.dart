a0600c55e63ded13cd159092ced9d5fc5c4c73ac
atef5922 <sifat15-5922@diu.edu.bd>
2025-04-11 14:26:23 +0600

    Task_4

---
+++ lib/task_4.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const TextStyleExample(),
+    );
+  }
+}
+
+class TextStyleExample extends StatelessWidget {
+  const TextStyleExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Text Styling Example:Task 04")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'Bold Text',
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Italic Text',
+              style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Colored Text',
+              style: TextStyle(fontSize: 22, color: Colors.blue),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Underlined Text',
+              style: TextStyle(
+                fontSize: 20,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Custom Font Size & Weight',
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
